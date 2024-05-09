d4b2f8f81d7e15fad4472974d377fa3525d172cf
Nahuel Rabey <nahuelrabeywork@gmail.com>
2024-05-09 17:12:12 -0300

    Tareas pendientes

--- app/src/main/java/savesplanner/Calculator.java
+++ app/src/main/java/savesplanner/Calculator.java
@@ -6,6 +6,10 @@ import org.apache.commons.math3.analysis.UnivariateFunction;
 
 class NewtonBinomial implements UnivariateFunction{
     public double value(double x){
+        /* 
+         * TODO:
+         * - [ ] Crear el polinomio a partir de un número de periodos
+         */
         double c = 100.0; // coefficient
         return -c + 3*c*x + 3*c*x*Math.pow(x, 2) + c*Math.pow(x, 3);
     }
@@ -13,6 +17,17 @@ class NewtonBinomial implements UnivariateFunction{
 
 public class Calculator {
     public static double futurePrice(double currentPrice, double interestRate, int periods){
+        /*
+         * FIXME:
+         * 
+         * Comportamiento extraño
+         * currentPrice: 100
+         * interesRate: 0.29803579673171043
+         * Salida: 218.7056528420159
+         * Salida esperada: 200
+         * 
+         * ¿Qué pasó?
+         */
         double price = currentPrice;
 
         for (int i = 1; i <= periods; i++){
@@ -23,6 +38,8 @@ public class Calculator {
     }
     public static double interestForPeriod(double finalInterest){
         NewtonBinomial function = new NewtonBinomial();
+
+        // Probemos luego con varios "solvers"
         BisectionSolver solver = new BisectionSolver();
         double res = solver.solve(1000, function, 0,1000);
         
